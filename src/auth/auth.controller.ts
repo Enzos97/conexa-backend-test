c266d0175fff6e24a84923c22c6879128ccb93fd
Enzos97 <enz997.ing.ind@gmail.com>
2023-09-25 12:55:28 -0300

    AuthGuard()

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 import { LoginUserDto } from './dto/login-user.dto';
-
+import { AuthGuard } from '@nestjs/passport'
+import { GetUser } from './decorators/get-user.decorator';
+import { User } from './entities/user.entity';
+import { RawHeader } from './decorators/raw-headers.decorator';
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
@@ -16,6 +19,22 @@ export class AuthController {
   @Post('login')
   login(@Body() loginUserDto: LoginUserDto){
     return this.authService.login(loginUserDto);
+  }
+
+  @Get('private')
+  @UseGuards(AuthGuard())
+  testinPrivateRoute(
+    @GetUser() user:User,
+    @GetUser('email') userEmail:string,
+    @RawHeader() rawHeaders: string[]
+  ){
+    return {
+      ok:true,
+      message: 'pedilo',
+      user,
+      userEmail,
+      rawHeaders
+    }
   }
   @Get()
   findAll() {
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -7,23 +7,28 @@ import { User } from './entities/user.entity';
 import { Model } from 'mongoose';
 import { CommonService } from 'src/common/common.service';
 import * as bcrypt from 'bcrypt'
-
+import { JwtPayload } from './interfaces/jwt-payload.interface';
+import { JwtService } from '@nestjs/jwt';
 @Injectable()
 export class AuthService {
   constructor(
     @InjectModel(User.name)
     private readonly userModel: Model<User>,
     private readonly commonService: CommonService,
+    private readonly jwtService: JwtService,
   ){}
 
   async create(createAuthDto: CreateAuthDto) {
     try {
       const {password, ...userData} = createAuthDto
       const newUser = await this.userModel.create({
-        ...userData,
+        userData,
         password: bcrypt.hashSync(password, 10)
       })
-      return newUser
+      return {
+        user: newUser,
+        token: this.getJwtToken({id :newUser.id})
+      }
     } catch (error) {
       this.commonService.handleExceptions(error)
     }
@@ -39,11 +44,20 @@ export class AuthService {
       if(!bcrypt.compareSync(password,user.password)){
         throw new UnauthorizedException('email or password is invalid')
       }
-      return user
+      return {
+        user,
+        token: this.getJwtToken({id :user.id})
+      }
     } catch (error) {
       this.commonService.handleExceptions(error)
     }
   }
+
+  private getJwtToken( payload:JwtPayload){
+    const token = this.jwtService.sign(payload);
+    return token
+  }
+
   findAll() {
     return `This action returns all auth`;
   }
